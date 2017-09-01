9835aacc130c935db46fc205e66a6f96e425bb96
Tom <thomasjackdalby@gmail.com>
2017-09-01 20:47:54 +0100

    got an initial window working

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ size = (700, 500)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("Shooty Bang Space Wars")
+
+clock = pygame.time.Clock()
+
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            screen.fill(WHITE)
+ 
+    pygame.display.flip()
+ 
+    clock.tick(60)
+ 
+pygame.quit()
