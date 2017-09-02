285c18f937d8cb4b24c0feb7846534cb74d9c452
Ben <ben.oxley@gmail.com>
2017-09-02 23:18:58 +0100

    Fixed dictionary keys

--- src/__main__.py
+++ src/__main__.py
@@ -110,7 +110,7 @@ class Program:
 
                 pygame.display.flip()
             except Exception:
-                self.running = false
+                self.running = False
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 traceback.print_exception(exc_type, exc_value, exc_traceback)
                 return
